a7fe36e2d38ae3f52c5e0232b59abcc56eb8bb59
Pietra <piietraaalvess@gmail.com>
2024-11-20 23:21:40 -0300

    update atualiza.py and db.sqlite3

--- FarmaCompare/core/management/commands/atualiza.py
+++ FarmaCompare/core/management/commands/atualiza.py
@@ -1,4 +1,3 @@
-
 import csv
 from decimal import Decimal, InvalidOperation
 from django.core.exceptions import ValidationError
@@ -21,20 +20,24 @@ class Command(BaseCommand):
             with open(os.path.join(data_folder, csv_file), newline='', encoding='utf-8') as f:
                 reader = csv.DictReader(f)
                 for row in reader:
-                    # Limpa o valor de 'price' para ser processado corretamente
-                    price_raw = row.get('price', '0').replace("R$", "").replace(".", "").replace(",", ".").strip()
+                    # Validação e limpeza do campo 'price'
+                    price_raw = row.get('price', '').replace("R$", "").replace(",", ".").strip()
+                    if not price_raw:  # Caso esteja vazio, atribuir 0 como padrão
+                        price_raw = "0"
                     try:
                         price = Decimal(price_raw)
                     except (ValueError, InvalidOperation):
                         raise ValidationError(f"Preço inválido: {row.get('price')}")
 
-                    # Limpa o valor de 'price_old' se existir
-                    price_old_raw = row.get('price_old', '0').replace("R$", "").replace(".", "").replace(",", ".").strip()
-                    try:
-                        price_old = Decimal(price_old_raw)
-                    except (ValueError, InvalidOperation):
+                    # Validação e limpeza do campo 'price_old'
+                    price_old_raw = row.get('price_old', '').replace("R$", "").replace(",", ".").strip()
+                    if not price_old_raw:  # Caso esteja vazio, definir como None
                         price_old = None
-                    print(row.keys())
+                    else:
+                        try:
+                            price_old = Decimal(price_old_raw)
+                        except (ValueError, InvalidOperation):
+                            price_old = None
                     produto, created = Produto.objects.update_or_create(
                         ean=row['ean'],
                         nome_farmacia=row['nome_farmacia'],
